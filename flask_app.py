d4cc677f39e07f10942146e1d720125865fe7851
rittwick bhabak <bhabakrittwick315@gmail.com>
2020-10-24 18:15:48 +0000

    cached username and password

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,17 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    with open('mysite/new.sh', 'w') as f:
+        f.write('#!/bin/bash')
+        f.write('\n')
+        f.write('echo hello')
+        f.close()
+
     return 'Hello from Flask!'
 
